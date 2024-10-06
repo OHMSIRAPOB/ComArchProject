e667dc8d00840c2f016f3cf9462fe4b0efc7b445
Okinawa <fitthep@gmail.com>
2024-10-06 17:45:51 +0700

    working on Tokenizer

--- src/tokenizer.java
+++ src/tokenizer.java
@@ -1,3 +1,16 @@
-public class tokenizer {
+import java.util.*;
 
+public class tokenizer {
+    List<String> line  ;
+
+    public int pos;
+    public Tokenizer(List<String> text){
+        pos = 0 ;
+        List<String> temp = new ArrayList<>();
+        for (String inputString : text) {
+//            List<String> tokens1 = tokenize(inputString);
+            temp.addAll(tokens1);
+        }
+        this.line = temp;
+    }
 }
